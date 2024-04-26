a40e79511f0c6e0fa14db609de23c5c7e0ddc94c
Ishwar749 <19itish083@ldce.ac.in>
2024-04-26 23:00:14 +0530

    Enable Actuator

--- src/main/java/com/ishwar/firstjobapp/review/ReviewController.java
+++ src/main/java/com/ishwar/firstjobapp/review/ReviewController.java
@@ -36,4 +36,29 @@ public class ReviewController {
         Review review = reviewService.getReviewById(companyId,reviewId);
         return new ResponseEntity<>(review, HttpStatus.OK);
     }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId, @PathVariable Long reviewId, @RequestBody Review review){
+
+        boolean isReviewUpdated = reviewService.updateReview(companyId,reviewId,review);
+        if(isReviewUpdated) {
+            return new ResponseEntity<>("Review updated successfully", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Review not updated",HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long companyId, @PathVariable Long reviewId){
+
+        boolean isReviewDeleted = reviewService.deleteReview(companyId, reviewId);
+
+        if(isReviewDeleted){
+            return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Review not deleted", HttpStatus.NOT_FOUND);
+        }
+    }
 }
